279d65f12f6ffc6754d7404fea3c50a742fde9ce
Jacob Yeung <xohkgolden-git@yahoo.com>
2025-05-19 23:25:50 -0700

    Enhance TSP implementation: add adjacency list creation and improve distance matrix display

--- src/main/java/AppPj02.java
+++ src/main/java/AppPj02.java
@@ -1,17 +1,21 @@
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
+
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class AppPj02 {
 
     public static double[][] distance;
     public static Map<String, City> cities;
+    public static Map<City, SortedSet<City>> sortedAdjacencylist = new TreeMap<>();
+    public static JFrame frameTSPDistanceMatrix;
 
     public static void main(String[] args) {
         System.out.println("Option 2 of the Course Project: \nCombinatorial Optimization - \nThe Traveling Salesperson Problem (TSP)");
         System.out.println("We have the following cities:");
-        System.out.println(cities = City.getCitiesInstance());
+        System.out.println(String.format("%s",cities = City.getCitiesInstance()).replace("),", "),\n"));
 
         System.out.println("The distance between the cities is given by the following matrix:");
         distance = new double[cities.size()][cities.size()];
@@ -32,8 +36,30 @@ public class AppPj02 {
             System.out.println();
         }
 
+        System.out.println("The distance matrix is shown in the GUI.");
         SwingUtilities.invokeLater(AppPj02::createDistanceGIU);
 
+        System.out.println("Create Adjacency list");
+        for(City city : cities.values()) {
+            SortedSet<City> adj = new TreeSet<>(new City.ComparatorCityDistance(city).reversed());
+            for(City city1 : cities.values()) {
+                if (city != city1)
+                    adj.add(city1);
+            }
+            sortedAdjacencylist.put(city, adj);
+        }
+
+        System.out.println("Adjacency list:");
+        for (City city : sortedAdjacencylist.keySet()) {
+            System.out.printf("%20s: ", city);
+
+            System.out.printf("%s \n", String.join( "<<",
+                    sortedAdjacencylist.get(city).stream()
+                            .filter(city1 -> city1 != city)
+                            .map(s->String.format("%20s ", s))
+                            .toArray(String[]::new)
+                    ));
+        }
     }
 
     public static void createDistanceGIU() {
@@ -55,7 +81,7 @@ public class AppPj02 {
             }
         }
 
-        JFrame frame = new JFrameTSPDistanceMatrix(tableData, columnNames);
+        frameTSPDistanceMatrix = new JFrameTSPDistanceMatrix(tableData, columnNames);
     }
 
 }
--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,32 +1,30 @@
-import java.util.LinkedHashMap;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class City implements Comparable<City> {
     private static final Map<String, City> cities =
             Map.of(
-            "A", new City("Central", "A", 0,0),
-                    "B", new City("Northville", "B", 2,8),
-                    "C", new City("Eastburg", "C", 6,3),
-                    "D", new City("Southtown", "D", 1,-5),
-                    "E", new City("Westend", "E", -4,3),
-                    "F", new City("Lakeside", "F", -3,-4)
+                    "A", new City("Central", "A", 0, 0),
+                    "B", new City("Northville", "B", 2, 8),
+                    "C", new City("Eastburg", "C", 6, 3),
+                    "D", new City("Southtown", "D", 1, -5),
+                    "E", new City("Westend", "E", -4, 3),
+                    "F", new City("Lakeside", "F", -3, -4)
             );
-
-    public static Map<String, City> getCitiesInstance() {
-        return new TreeMap<>(cities);
-    }
-
     private final String name;
     private final String label;
     private final int x;
     private final int y;
-
-    public City(String name, String label,  int x, int y) {
+    public City(String name, String label, int x, int y) {
         this.name = name;
         this.label = label;
         this.x = x;
         this.y = y;
+    }
+
+    public static Map<String, City> getCitiesInstance() {
+        return new TreeMap<>(cities);
     }
 
     public String getName() {
@@ -51,7 +49,7 @@ public class City implements Comparable<City> {
 
     @Override
     public String toString() {
-        return String.format("%s %s (%d, %d)", name, label , x, y);
+        return String.format("%s %s (%d, %d)", name, label, x, y);
     }
 
     public double distanceTo(City other) {
@@ -59,7 +57,21 @@ public class City implements Comparable<City> {
     }
 
     public int compareTo(City other) {
-        return (int) Math.abs(this.distanceTo(other) * 100);
+        return CharSequence.compare(this.label, other.label);
     }
 
+    public static class ComparatorCityDistance implements Comparator<City> {
+
+        private final City defaultCity;
+
+        public ComparatorCityDistance(City defaultCity) {
+            this.defaultCity = defaultCity;
+        }
+
+        @Override
+        public int compare(City o1, City o2) {
+            int i = Double.compare(defaultCity.distanceTo(o2), defaultCity.distanceTo(o1));
+            return i != 0 ? i : CharSequence.compare(o1.label, o2.label);
+        }
+    }
 }
