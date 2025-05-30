b2a95f657c2984618fc5ef67d80ef15c45e9a3a0
Jacob Yeung <xohkgolden-git@yahoo.com>
2025-05-30 14:55:16 -0700

    Add test for GenericTreeSet: implement city management and display functionality

---
+++ src/test/java/TestGenericTreeSet.java
@@ -0,0 +1,32 @@
+public class TestGenericTreeSet {
+
+    public static void main(String[] args) {
+        GenericTreeSet treeSet = new GenericTreeSet();
+
+        City cityA = new City("City A", 'A', 1, 2);
+        City cityC = new City("City C", 'C', 5, 6);
+        City cityD = new City("City D", 'D', -1, -1);
+        City cityE = new City("City E", 'E', -2, -2);
+        City cityB = new City("City B", 'B', 3, 4);
+        City cityK = new City("City K", 'K', -6, -6);
+
+        City cityF = new City("City F", 'F', 7, 8);     // 東北方，遠離其他城市
+        City cityG = new City("City G", 'G', 4, 3);     // 介於 B 和 C 之間
+        City cityH = new City("City H", 'H', -4, -5);   // 貼近 K
+        City cityI = new City("City I", 'I', 0, 0);     // 靠近 A 和 D 的中間
+        City cityJ = new City("City J", 'J', 2, 1);     // 靠近 A 和 B
+        City cityL = new City("City L", 'L', -3, -2);   // 介於 E 和 H 之間
+        City cityM = new City("City M", 'M', 6, 7);     // 介於 C 和 F 之間
+        City cityN = new City("City N", 'N', -7, -8);   // 更遠的西南方
+        City cityO = new City("City O", 'O', 5, 3);     // 靠近 G，但偏東
+
+
+
+        treeSet.addAll(cityA, cityB, cityC, cityD, cityE, cityF, cityG, cityH,
+                cityI, cityJ, cityK, cityL, cityM, cityN, cityO);
+
+        for (City city : treeSet) {
+            System.out.println(city);
+        }
+    }
+}
